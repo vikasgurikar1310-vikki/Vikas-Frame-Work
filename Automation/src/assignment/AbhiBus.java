6c72f1c50a81497a0cadd31901b267cb1169011a
vikasgurikar1310-vikki <vikasgurikar1310@gmail.com>
2026-05-05 11:35:47 +0530

    Update AbhiBus.java

--- Automation/src/assignment/AbhiBus.java
+++ Automation/src/assignment/AbhiBus.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AbhiBus {
 public static void main(String[] args) {
 	WebDriver driver = new ChromeDriver();
-	driver.manage().window().maximize();
+	driver.manage().window().minimize();
 	driver.get("https://www.abhibus.com/");
 	driver.findElement(By.xpath("(//a[contains(@class,'btn text-neutral-800 light')]/../../../..//div[@class=\"h5 col\"])[1]")).click();
 	
